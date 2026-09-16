c1242755e88cb8c8cbd79406fb0ff12d636e1132
kstzv <kstzavertaylo@gmail.com>
2026-09-16 13:00:24 +0300

    Update shake.h

--- portable/src/shake.h
+++ portable/src/shake.h
@@ -16,7 +16,7 @@ struct shake_ctx {
     size_t outlen;
 };
 
-int shake_ctx_get_mem(struct shake_ctx *ctx, uint64_t *state, size_t size_mem)
+int shake_ctx_get_mem(struct shake_ctx *ctx, uint64_t *state, size_t size_mem);
 void shake_ctx_init(struct shake_ctx *ctx,uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen);
 void shake_ctx_zero(struct shake_ctx *ctx);
 void shake128(struct shake_ctx *ctx);
