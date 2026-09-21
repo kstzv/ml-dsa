375e522a086728373e6be8b39b16baa03ae8784c
kstzv <kstzavertaylo@gmail.com>
2026-09-21 16:45:43 +0300

    Update ml_dsa_create_c.c

--- portable/src/ml_dsa_create_c.c
+++ portable/src/ml_dsa_create_c.c
@@ -130,9 +130,24 @@ void ml_dsa_expand_mask(struct ml_dsa_keys *ctx, s32 *y, u32 kappa)
 	}
 }
 
-static inline void ml_dsa_decompose(s32 r, s32 gamma2, s32 *r1, s32 *r0)
+void ml_dsa_get_w1(struct ml_dsa_keys *ctx)
 {
-    s32 high = (r + 127) >> 7;
+	s32 gamma2;
+	if(ctx->k == ML_DSA_44_K) { gamma2 = ML_DSA_44_GAMMA2; }
+	else { gamma2 = ML_DSA_65_87_GAMMA2; }
+	
+	for(size_t i = 0; i < ctx->k; i++)
+	{
+		for(size_t j = 0; j < ML_DSA_N; j++)
+		{
+			ml_dsa_decompose(ctx->workspace->vect_w + (i * ML_DSA_N) + j, gamma2, ctx->workspace->temp_vector_buffer + (i * ML_DSA_N) + j, NULL);
+		}
+	}
+}
+
+static inline void ml_dsa_decompose(s32 *r, s32 gamma2, s32 *r1, s32 *r0)
+{
+    s32 high = (*r + 127) >> 7;
 
     if (gamma2 == ML_DSA_65_87_GAMMA2) 
     {
@@ -144,11 +159,59 @@ static inline void ml_dsa_decompose(s32 r, s32 gamma2, s32 *r1, s32 *r0)
         high ^= ((43 - high) >> 31) & high;
     }
 
-    s32 low = r - high * 2 * gamma2;
+    s32 low = *r - high * 2 * gamma2;
     low -= ((ML_DSA_Q_HALF_MINUS_ONE - low) >> 31) & ML_DSA_Q;
 
-    *r1 = high;
-    *r0 = low;
+    if (r1 != NULL) { *r1 = high; }
+	if (r0 != NULL) { *r0 = low; }
+}
+
+void ml_dsa_get_c(struct ml_dsa_keys *ctx, u8 *c)
+{
+	size_t counter_bytes = 0;
+	memcpy(ctx->workspace->scratch_buffer, ctx->workspace->mu, ML_DSA_64_BYTES);
+	counter_bytes += ML_DSA_64_BYTES;
+	
+	if(ctx->k == ML_DSA_44_K) { counter_bytes += pack_for_c_44(ctx); }
+	else { counter_bytes += pack_for_c_65_87(ctx); }
+	
+	size_t size_c = 0;
+	if(ctx->k == ML_DSA_44_K) { size_c = ML_DSA_32_BYTES; }
+	else if(ctx->k == ML_DSA_65_K) { size_c = ML_DSA_32_BYTES + 16; }
+	else if(ctx->k == ML_DSA_87_K) { size_c = ML_DSA_64_BYTES; }
+	
+	shake_ctx_zero(ctx->workspace->shake);
+	shake_ctx_init(ctx->workspace->shake, c, size_c, ctx->workspace->scratch_buffer, counter_bytes);
+	shake256(ctx->workspace->shake);
+}
+	
+			
+static inline size_t pack_for_c_44(struct ml_dsa_keys *ctx)
+{
+    size_t counter_bytes = 0;
+    u8 *temp_buff = ctx->workspace->scratch_buffer + ML_DSA_64_BYTES;
+    const s32 *w = ctx->workspace->temp_vector_buffer;
+
+    for (size_t i = 0; i < (size_t)ctx->k * ML_DSA_N; i += 4)
+    {
+        temp_buff[counter_bytes++] = (u8)(w[i] | (w[i + 1] << 6));
+        temp_buff[counter_bytes++] = (u8)((w[i + 1] >> 2) | (w[i + 2] << 4));
+        temp_buff[counter_bytes++] = (u8)((w[i + 2] >> 4) | (w[i + 3] << 2));
+    }
+    return counter_bytes;
+}
+
+static inline size_t pack_for_c_65_87(struct ml_dsa_keys *ctx)
+{
+    size_t counter_bytes = 0;
+    u8 *temp_buff = ctx->workspace->scratch_buffer + ML_DSA_64_BYTES;
+    const s32 *w = ctx->workspace->temp_vector_buffer;
+
+    for (size_t i = 0; i < (size_t)ctx->k * ML_DSA_N; i += 2)
+    {
+        temp_buff[counter_bytes++] = (u8)(w[i] | (w[i + 1] << 4));
+    }
+    return counter_bytes;
 }
 	
 		
